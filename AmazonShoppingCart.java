76d166df39098150dbc83c27e4393f2c97bfa03e
sriyanmadugula <84945150+sriyanmadugula@users.noreply.github.com>
2021-06-01 11:30:04 -0400

    Add files via upload

---
+++ AmazonShoppingCart.java
@@ -0,0 +1,32 @@
+public class AmazonShoppingCart
+{
+    public static void main(String[] args)
+    {   int TVQuantity = 1;
+        double TVPrice = 2499.99;
+        String TVName = "Samsung 65 in Full HD TV";
+        
+        int phoneQuantity = 1;
+        double phonePrice = 799.99;
+        String phoneName = "iPhone 12";
+        
+        int penQuantity = 10;
+        double penPrice = 1.99;
+        String penName = "BIC Ballpoint Pens";
+        
+        int bedQuantity = 1;
+        double bedPrice = 749.99;
+        String bedName = "LUCID Twin Size Bed";
+        
+        int pillowQuantity = 5;
+        double pillowPrice = 22.99;
+        String pillowName = "HIMOON Bed Pillows";
+        
+        double totalBeforeTax = TVQuantity*TVPrice + phoneQuantity*phonePrice + penQuantity*penPrice + bedQuantity*bedPrice + pillowQuantity*pillowPrice;
+        double totalAfterTax = 1.06*totalBeforeTax;
+        System.out.println("Your cart total is: $" + totalBeforeTax + ".");
+        System.out.println("Your cart total after tax is: $" + totalAfterTax + ".");
+        System.out.println("Your items include: " + TVQuantity + " " + TVName + "," + " " + phoneQuantity + " " + phoneName + "," + " " + penQuantity + " " + penName + "," + " " + bedQuantity + " " + bedName + "," + " and " + pillowQuantity + " " + pillowName + "."); 
+        
+     
+    }
+}    
