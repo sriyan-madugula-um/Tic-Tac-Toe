83b47bbfd297c28e8f787edd7f7324208bd5ae57
sriyanmadugula <84945150+sriyanmadugula@users.noreply.github.com>
2021-06-03 12:31:16 -0400

    Update TicTacToeGameBoard.java

--- TicTacToeGameBoard.java
+++ TicTacToeGameBoard.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class TicTacToeGameBoard
 {
     public static void main(String[] args)
@@ -18,5 +19,71 @@ public class TicTacToeGameBoard
             }
             System.out.println();
         }
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number between 1-9 for your position");
+        int position = input.nextInt();
+        
+        char variable = ' ';
+        for (int i = 0; i < 2 /*need to make a variable for this*/; i++)
+        {
+            if (i % 2 == 0)
+            {
+                variable = 'o';
+            }
+            else
+            {
+                variable = 'x';
+            }
+            if (position == 1)
+            {
+                gameBoard[0][0] = variable;
+            }
+            if (position == 2)
+            {
+                gameBoard[0][2] = variable;
+            }
+            if (position == 3)
+            {
+                gameBoard[0][4] = variable;
+            }
+            if (position == 4)
+            {
+                gameBoard[2][0] = variable;
+            }
+            if (position == 5)
+            {
+                gameBoard[2][2] = variable;
+            }
+            if (position == 6)
+            {
+                gameBoard[2][4] = variable;
+            }
+            if (position == 7)
+            {
+                gameBoard[4][0] = variable;
+            }
+            if (position == 8)
+            {
+                gameBoard[4][2] = variable;
+            }
+            if (position == 9)
+            {
+                gameBoard[4][4] = variable;
+            }
+        }
+        
+        for(int i = 0; i < 5; i++)
+        {
+            char[] row = gameBoard[i];
+            for(int j = 0; j < 5; j++)
+            {
+                char c = row[j];
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
     }
+    
 }
